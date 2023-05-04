a235698f32bf67182af13969ddb5256f9f20ffa7
dzoric1 <Gerasimov2099@mail.ru>
2023-05-04 22:07:31 +0800

    small fix

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,28 +1,4 @@
 export const
-  formEdit = document.querySelector('.popup__form_type_edit'),
-  formAdd = document.querySelector('.popup__form_type_add'),
-  profileEditButton = document.querySelector('.profile__edit-button'),
-  profileAddButton = document.querySelector('.profile__add-button'),
-  inputName = document.querySelector('.popup__form-input_type_name'),
-  inputJob = document.querySelector('.popup__form-input_type_job'),
-  profileAvatarImg = document.querySelector('.profile__avatar'),
-  avatarEditButton = document.querySelector('.profile__avatar-wrapper'),
-  validateSettings = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__form-input',
-    submitButtonSelector: '.popup__submit',
-    inactiveButtonClass: 'popup__submit_type_disabled',
-    inputErrorClass: 'popup__form-input_type_error',
-  },
-  profileSelectors = {
-    profileNameSelector: '.profile__name',
-    profileJobSelector: '.profile__work'
-  },
-  imagePopupSelectors = {
-    popupSelector: '.popup_type_image',
-    imageSelector: '.popup__image',
-    textSelector: '.popup__location'
-  },
   apiSettings = {
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-64',
     headers: {
